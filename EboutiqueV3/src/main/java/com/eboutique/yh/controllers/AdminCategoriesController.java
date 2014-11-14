b1c93039db7772780ecfad47a1fdb4a6c53ce749
youssef harkati <youss.hark@gmail.com>
2014-11-14 15:02:58 +0100

    Finished admin section for category creation with pic also

--- EboutiqueV3/src/main/java/com/eboutique/yh/controllers/AdminCategoriesController.java
+++ EboutiqueV3/src/main/java/com/eboutique/yh/controllers/AdminCategoriesController.java
@@ -1,9 +1,21 @@
 package com.eboutique.yh.controllers;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.validation.Valid;
+
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.eboutique.yh.entities.Categorie;
 import com.eboutique.yh.metier.IAdminCategoriesMetier;
@@ -13,6 +25,7 @@ import com.eboutique.yh.metier.IAdminCategoriesMetier;
 public class AdminCategoriesController {
    @Autowired
    private IAdminCategoriesMetier metier;
+   
    @RequestMapping(value="/index")
    public String index(Model model){
 	   model.addAttribute("categorie", new Categorie());
@@ -20,4 +33,35 @@ public class AdminCategoriesController {
 	return "categories";
 	   
    }
+   
+   @RequestMapping(value="/saveCat")
+   public String saveCat(@Valid Categorie c,BindingResult bindingResult, //gestion erreur validation
+		        Model model, MultipartFile file) throws IOException{
+	  if(bindingResult.hasErrors()){
+		  model.addAttribute("categories",metier.listCategories());
+		  
+		  return("categories");
+	  }
+	  
+	  if(!file.isEmpty()) {
+		  BufferedImage bi = ImageIO.read(file.getInputStream());
+		  c.setPhoto(file.getBytes());
+		  c.setNomPhoto(file.getOriginalFilename());
+	  }
+	  
+	  metier.ajouterCategorie(c);
+	   
+	   model.addAttribute("categorie", new Categorie());
+	   model.addAttribute("categories",metier.listCategories());
+	return "categories";
+	   
+   }
+   
+   @RequestMapping(value="photoCat", produces=MediaType.IMAGE_JPEG_VALUE)
+   @ResponseBody
+   public byte[] photoCat(Long idCat) throws IOException{
+	  Categorie c = metier.getCategorie(idCat); 
+	  return IOUtils.toByteArray(new ByteArrayInputStream(c.getPhoto())) ;
+   }
+   
 }
